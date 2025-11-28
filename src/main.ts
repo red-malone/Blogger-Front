95c6df8282684ac6a4755a4232796069ed7e3975
red-malone <rohit.psudarsanan@gmail.com>
2025-11-29 00:26:51 +0530

    dotenv config addition

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
