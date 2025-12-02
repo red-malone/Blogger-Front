1bd3948c270dad4ba314c179db695444ad7fa8b5
red-malone <rohit.psudarsanan@gmail.com>
2025-12-02 22:58:55 +0530

    route fix

--- src/app/shared/components/top-bar/top-bar.ts
+++ src/app/shared/components/top-bar/top-bar.ts
@@ -3,14 +3,20 @@ import { Component, Input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { AuthService } from '../../../services/auth.service';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './top-bar.html',
   styleUrls: ['./top-bar.scss']
 })
 export class TopBarComponent {
   @Input() isLoggedIn: boolean = false;
+  constructor(private auth: AuthService) {}
+
+  get isGuest(): boolean {
+    return !this.auth.isLoggedIn();
+  }
 }
