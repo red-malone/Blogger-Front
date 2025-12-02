aba96f9e4792485cbeefe41ec2e1ae3f099ba147
red-malone <rohit.psudarsanan@gmail.com>
2025-12-02 23:25:56 +0530

    comments and style change

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+const API_URL = environment?.apiUrl?.replace(/\/\/$/, '') || 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
-export class User {
-  private api = 
-  // process.env.API_URL || 
-  'http://localhost:3000/api';
 
+export class User {
+  constructor(private http: HttpClient) {}
   
+  getProfile() {
+    return this.http.get(`${API_URL}/user/profile`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,8 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+  //TODO:Check the url pathway if written correctly
+  //TODO:Check if the appConfig is imported correctly
+  //TODO:TEst the api calls in user service
